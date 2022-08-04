181edb8ee970d7c1114767f2b679b718888bd423
vcarter3 <121vcarter@gmail.com>
2022-08-05 00:21:46 +0100

    added button functionality

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = false
 
-    this.info = function () {
+    this.printTitle = function () {
         return this.title
+    }
+    this.printAuthor = function () {
+        return this.author
+    }
+    this.printPages = function () {
+        return this.pages
+    }
+    this.printRead = function () {
+        return this.read
     }
 }
 
@@ -21,35 +30,87 @@ function addBookToLibrary(...books) {
 }
 
 
-const page = document.querySelector("body > p1");
+const libraryShelf = document.querySelector("body > div");
 
-const bookShelf = document.querySelector("body > div");
+
+function createBook(title,author,pages,read){
+
+    const parent = document.createElement("div");
+    const child1 = document.createElement("h3");
+    child1.textContent = title;
+    parent.appendChild(child1);
+
+
+    //<p>by <span class="author">AuthorName</span></p>
+
+    const child2 = document.createElement("p");
+    child2.textContent = "By ";
+
+    const grandChild = document.createElement("span");
+    grandChild.className = "author";
+    grandChild.textContent = author;
+    child2.appendChild(grandChild);
+    parent.appendChild(child2);
+
+    //<p class="details"><span class="bold">Length: </span>288 pages</p>
+
+    const child3 = document.createElement("p");
+    child3.textContent = "Pages: ";
+    const grandChild2 = document.createElement("span");
+    grandChild2.className = "pages";
+    grandChild2.textContent = pages;
+    child3.appendChild(grandChild2);
+    parent.appendChild(child3);
+
+
+    //<p class="details"><span class="bold">Status: </span><span class="done">Done</span></p>
+
+    const child4 = document.createElement("p");
+    child4.textContent = "Status: ";
+    const grandChild3 = document.createElement("button");
+    grandChild3.className = "read";
+    if(read){
+        grandChild3.textContent = "Done";
+    }else{
+        grandChild3.textContent = "Not done";
+    }
+    
+    child4.appendChild(grandChild3);
+    parent.appendChild(child4);
+
+    //<button>bin</button>
+
+    const child5 = document.createElement("button");
+    child5.textContent = "bin me";
+    child5.className = "bin";
+    parent.appendChild(child5);
+
+
+    // send book to shelf
+    //libraryShelf.appendChild(parent);
+
+
+    //send book to front of shelf
+
+    const firstBook = document.querySelector(".libraryDisplay div:first-of-type");
+    libraryDisplay.insertBefore(parent, firstBook);
+
+
+    // remove button event listener
+    const buttonRemove = document.querySelector(".libraryDisplay > div:first-of-type > button");
+    buttonRemove.addEventListener('click',
+        (e) => {
+            removeElement(e);
+        });
+
+
+}
 
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-
-        // for each book in the lib
-        // create a new div
-        const bookContainer = document.createElement("div");
-        // Fill it with content and structure
-        const bookTitle = bookContainer.createElement("h2");
-        const bookContent = bookTitle.createTextNode(myLibrary[i].info());
-        
-        
-        
-        bookContainer.appendChild(newCoontent);
-        libraryDisplay.appendChild(titleContainer);
-
-        // create a new h2 element
-        const titleContainer = document.createElement("h2");
-        // and give it some content
-        const newCoontent = document.createTextNode(myLibrary[i].info());
-        titleContainer.appendChild(newCoontent);
-        libraryDisplay.appendChild(titleContainer);
-        
+        createBook(myLibrary[i].printTitle(),myLibrary[i].printAuthor(),myLibrary[i].printPages(),myLibrary[i].printRead());
     }
 }
-
 
 
 const add = document.querySelector('#add');
@@ -59,33 +120,26 @@ const libraryDisplay = document.querySelector('.libraryDisplay');
 
 // event listeners 
 function removeElement(e) {
+
+    // remove button + event listener?
     console.log(e.target);
     e.target.parentElement.remove();
+
+    // need to remove book from myLibrary 
+
+    console.log(myLibrary);
 };
 
 
 function addElement() {
-    // create a new div element
-    const newDiv = document.createElement("div");
-    // and give it some content
-    const newContent = document.createTextNode("New book");
-    const button = document.createElement("button");
-    button.innerHTML = "remove me";
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-    newDiv.appendChild(button);
-    // add the newly created element and its content into the DOM
-    libraryDisplay.appendChild(newDiv);
 
-    const buttonRemove = document.querySelector(".libraryDisplay div:last-child > button");
+    // need to get user input
 
-    buttonRemove.addEventListener('click',
-        (e) => {
-            removeElement(e);
-        });
+    // need to add book to myLibrary 
 
 
-
+    // display book
+    createBook("title","author",12,false);
 }
 
 add.addEventListener('click', (e) => {
